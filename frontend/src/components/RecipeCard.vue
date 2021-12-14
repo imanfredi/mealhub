aacf9120dce0858262b72902cc8e666a8e89eb77
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2021-12-14 13:17:21 -0300

    feat: RecipeCard and Order Headers

---
+++ frontend/src/components/RecipeCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card elevation="2" outlined class="d-flex-column grow">
+    <v-card-title>Recipe</v-card-title>
+    <v-card-subtitle>The best recipe in the world</v-card-subtitle>
+    <v-card-text class="d-flex justify-end"
+      ><b class="mx-1">Calories</b> 150 <b class="mx-1">Protein</b> 5g
+      <b class="mx-1">Carbs</b> 10g <b class="mx-1">Sugar</b> 2g
+      <b class="mx-1">Minutes</b> 25</v-card-text
+    >
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "RecipeCard",
+};
+</script>
+
+<style>
+.grow {
+  transition: all 0.15s ease-in-out;
+}
+.grow:hover {
+  transform: scale(1.01);
+  cursor: pointer;
+}
+</style>
--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -2,10 +2,22 @@
   <v-container fill-height fluid class="bgImage">
     <v-row>
       <v-col cols="5">
-        <v-card width="800px" height="200px" color="#F1FAEE" class="ml-16">
+        <v-card color="#F1FAEE" class="ml-16 pt-5 px-4">
           <v-container fill-height fuild>
             <v-row>
-              <v-col cols="12">
+              <v-col cols="12" class="pb-0 pt-0">
+                <p class="text-center font-weight-medium strong mb-2">
+                  Ingredients
+                </p>
+              </v-col>
+            </v-row>
+            <v-row align="center" justify="center">
+              <v-col cols="4" class="pt-0">
+                <hr style="margin: auto" />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" class="pb-0 pt-0">
                 <v-autocomplete
                   chips
                   clearable
@@ -17,14 +29,63 @@
               </v-col>
             </v-row>
             <v-row>
-              <v-col cols="12"> </v-col>
+              <v-col cols="12" class="pb-0 pt-0">
+                <p class="text-center font-weight-medium strong mb-2">
+                  Not Ingredients
+                </p>
+              </v-col>
+            </v-row>
+            <v-row align="center" justify="center">
+              <v-col cols="4" class="pt-0">
+                <hr style="margin: auto" />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12" class="pb-0 pt-0">
+                <v-autocomplete
+                  chips
+                  clearable
+                  deletable-chips
+                  multiple
+                  small-chips
+                  solo
+                ></v-autocomplete>
+              </v-col>
             </v-row>
           </v-container>
         </v-card>
       </v-col>
-      <v-col cols="7">
-        <v-card width="800px" height="600px" color="#F1FAEE" class="ml-16">
-          <v-card-text> Centered </v-card-text>
+      <v-col cols="6" class="d-flex-column align-center">
+        <v-card height="600px" color="#F1FAEE">
+          <v-container>
+            <v-row>
+              <v-col cols="12" class="pt-7">
+                <v-data-table
+                  :headers="headers"
+                  class="elevation-1"
+                  disable-filtering
+                  disable-pagination
+                  hide-default-footer
+                  no-data-text
+                ></v-data-table>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <RecipeCard></RecipeCard>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <RecipeCard></RecipeCard>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <RecipeCard></RecipeCard>
+              </v-col>
+            </v-row>
+          </v-container>
         </v-card>
       </v-col>
     </v-row>
@@ -32,14 +93,33 @@
 </template>
 
 <script>
+import RecipeCard from "../components/RecipeCard.vue";
+
 export default {
+  data() {
+    return {
+      headers: [
+        { text: "Calories", value: "calories" },
+        { text: "Protein (g)", value: "protein" },
+        { text: "Carbs (g)", value: "carbs" },
+        { text: "Sugar (g)", value: "sugar" },
+        { text: "Minutes", value: "minutes" },
+      ],
+    };
+  },
   name: "LandingPage",
+  components: {
+    RecipeCard,
+  },
 };
 </script>
 
-<style scoped>
+<style>
 .bgImage {
   background-image: url("../assets/images/background_image.jpg");
   background-size: cover;
+}
+.v-data-table__empty-wrapper {
+  display: none !important;
 }
 </style>
